f8b4e245f10dd2dc34d0ad9da3fa67482e439d8f
Chandu Weerakoon <chandu.weerakoon@dbresults.com.au>
2023-07-19 17:21:39 +1000

    modified required files

--- main.py
+++ main.py
@@ -13,16 +13,19 @@ root_dir = os.path.dirname(current_dir)
 # Usage
 file_path = 'tests/test.csv'
 csv_data = read_csv_file(file_path)
+print(csv_data)
 
 # storing records in memory as python objects
 person_list = []
 names_list = []
 for row in csv_data:
     person_list.append(row) # now person_list contains a list of each of the Person objects
+print(person_list)
 
 # read in the json file using the util function and print to stdout
 file_path = 'config.json'  # Specify the path to the JSON file
 json_dict = read_json_file(file_path)  # Call the function to read and parse the JSON file
+print(json_dict)
 
 # create a cursor object to execute queries
 cursor = conn.cursor()
